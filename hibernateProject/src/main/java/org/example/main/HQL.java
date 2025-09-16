a1bf322eb29d4449796525c5e51d3f06f67012f8
Suresh Nani <sureshslvr@gmail.com>
2025-09-16 19:25:12 +0530

    HQL DML operations practice(insert update delete)

--- hibernateProject/src/main/java/org/example/main/HQL.java
+++ hibernateProject/src/main/java/org/example/main/HQL.java
@@ -3,6 +3,7 @@ package org.example.main;
 import org.example.pojo.Employee;
 import org.example.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.query.MutationQuery;
 import org.hibernate.query.SelectionQuery;
 
 import java.util.List;
@@ -11,8 +12,9 @@ public class HQL {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         try(session){
-            findByParameter(session);
-
+            //insert(session);
+            //update(session);
+            delete(session);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }finally {
@@ -21,6 +23,34 @@ public class HQL {
 
 
     }
+
+    private static void delete(Session session) {
+        session.beginTransaction();
+        MutationQuery query = session.createMutationQuery("delete from Employee where empId=?1");
+        query.setParameter(1,19);
+        query.executeUpdate();
+        session.getTransaction().commit();
+    }
+
+    private static void update(Session session) {
+        session.beginTransaction();
+        MutationQuery query = session.createMutationQuery("update Employee set salary=?1 where empId=?2");
+        query.setParameter(1,40000);
+        query.setParameter(2, 17);
+        query.executeUpdate();
+        session.getTransaction().commit();
+    }
+
+    private static void insert(Session session) {
+        session.beginTransaction();
+        MutationQuery query = session.createMutationQuery("insert into Employee(name,position,salary) values(?1,?2,?3)");
+        query.setParameter(1,"haodihf");
+        query.setParameter(2,"security");
+        query.setParameter(3,20000);
+        query.executeUpdate();
+        session.getTransaction().commit();
+    }
+
     private static void findByParameter(Session session) {
         SelectionQuery<Employee> query = session.createSelectionQuery("select e from Employee e where empId=?1", Employee.class);
         query.setParameter(1,3);
