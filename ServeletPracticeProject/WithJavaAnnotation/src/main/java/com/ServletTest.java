cd718876ef94d7963ff17c6013d4d3b0835a6fc2
Suresh Nani <sureshslvr@gmail.com>
2025-09-23 17:48:13 +0530

    servlet project with out web.xml doen with java annotation

--- ServeletPracticeProject/WithJavaAnnotation/src/main/java/com/ServletTest.java
+++ ServeletPracticeProject/WithJavaAnnotation/src/main/java/com/ServletTest.java
@@ -1,6 +1,7 @@
-package  com;
+package com;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,12 +9,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class FirstServlet extends HttpServlet {
-
+@WebServlet("/servletTest")
+public class ServletTest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
-        writer.print("hi this is first project setup");
+        writer.print("this is from servletTest servlet without web.xml");
     }
 }
