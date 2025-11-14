f5119369951b03f4a24664879ec7895c026bedf7
sureshslvr <102788617+sureshslvr@users.noreply.github.com>
2025-11-14 10:55:38 +0530

    Create StudentTest.java

---
+++ SpringPrctice/SpringCore/SpringCorexml/src/main/java/com/xmlwithautowire/StudentTest.java
@@ -0,0 +1,12 @@
+package org.xmlwithautowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("appconfigwithjavaannotations.xml");
+        Student student = applicationContext.getBean("student", Student.class);
+        System.out.println(student);
+    }
+}
