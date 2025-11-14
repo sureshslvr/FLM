60c1524be06b11584d2ea283ead3ee26a1cd9b93
sureshslvr <102788617+sureshslvr@users.noreply.github.com>
2025-11-14 10:54:41 +0530

    Create Address.java

---
+++ SpringPrctice/SpringCore/SpringCorexml/src/main/java/com/xmlwithautowire/Address.java
@@ -0,0 +1,16 @@
+package org.xmlwithautowire;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class Address {
+    @Value("${address}")
+    private String address;
+}
