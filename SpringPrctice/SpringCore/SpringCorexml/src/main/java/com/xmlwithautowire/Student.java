655208e180da892748b93918eddf002731da8774
sureshslvr <102788617+sureshslvr@users.noreply.github.com>
2025-11-14 10:55:14 +0530

    Create Student.java

---
+++ SpringPrctice/SpringCore/SpringCorexml/src/main/java/com/xmlwithautowire/Student.java
@@ -0,0 +1,23 @@
+package org.xmlwithautowire;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//lambok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//spring bean
+@Component
+public class Student {
+    @Value("01")
+    private int studentId;
+    @Value("suresh")
+    private String studentName;
+    @Autowired
+    private Address address;
+}
